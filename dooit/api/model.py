1a6efce1853188597fd45600a0a0fedc898eee95
ragaathorn <ragaathorn@gmail.com>
2025-12-11 20:28:20 -0500

    added a sibling node function

--- dooit/api/model.py
+++ dooit/api/model.py
@@ -124,6 +124,9 @@ class DooitModel(BaseModel, BaseModelMixin):
     def _add_sibling(self) -> Self:
         raise NotImplementedError  # pragma: no cover
 
+    def _add_sibling_node(self) -> Self:
+        raise NotImplementedError  # pragma: no cover
+
     def add_sibling(self):
         return self._add_sibling()
 
