77ccc1d92b45aee0fabbb1bf054c655f9bd9edc1
watermelonpapa <beyondjobs@qq.com>
2026-01-13 23:27:42 +0800

    fix: add missing effort key description in HelpScreen

--- dooit/ui/api/dooit_api.py
+++ dooit/ui/api/dooit_api.py
@@ -150,6 +150,7 @@ class DooitAPI:
         return self.edit("recurrence")
 
     def edit_effort(self):
+        """Start editing the effort of the todo"""
         return self.edit("effort")
 
     def add_sibling(self):
