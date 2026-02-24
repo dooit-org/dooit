fc85e84975d83b9c6d063b906d365db061139d52
kraanzu <kraanzu@gmail.com>
2026-02-24 21:56:03 +0530

    Remove DooitThemeBase

--- dooit/ui/bridge/components/vars.py
+++ dooit/ui/bridge/components/vars.py
@@ -2,8 +2,8 @@ from typing import TYPE_CHECKING, Optional
 
 from textual.widgets import ContentSwitcher
 
+from dooit.config.config import ThemeColors
 from dooit.models import Workspace
-from dooit.models.theme import DooitThemeBase
 from dooit.models.todo import Todo
 from dooit.ui.widgets.trees import TodosTree, WorkspacesTree
 
@@ -26,7 +26,7 @@ class VarManager(ApiComponent):
         return self.api.app.dooit_mode
 
     @property
-    def theme(self) -> DooitThemeBase:
+    def theme(self) -> ThemeColors:
         return self.api.css.theme
 
     @property
