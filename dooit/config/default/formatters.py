8b07ccbcea45d6961b6189968b387025cfedae52
kraanzu <kraanzu@gmail.com>
2026-02-26 12:45:33 +0530

    Better default config

--- dooit/config/default/formatters.py
+++ dooit/config/default/formatters.py
@@ -1,29 +1,87 @@
+from rich.style import Style
+from rich.text import Text
+
 from dooit.models import Todo, Workspace
 
 
-def workspace_description(workspace: Workspace, **kwargs):
-    return str(workspace.description)
+def build_style(css: dict) -> Style:
+    return Style(
+        color=css.get("color"),
+        bgcolor=css.get("background"),
+        bold=css.get("bold"),
+        italic=css.get("italic"),
+    )
 
 
-def todo_description(todo: Todo, **kwargs):
-    return str(todo.description)
+def workspace_description(workspace: Workspace, context: dict) -> Text:
+    if child_count := len(workspace.workspaces):
+        text = context["format_with_child"].format(
+            description=workspace.description, child_count=child_count
+        )
+    else:
+        text = context["format"].format(description=workspace.description)
+
+    return Text(text)
 
 
-def todo_status(todo: Todo, **kwargs):
-    return str(todo.status)
+def todo_description(todo: Todo, context: dict) -> Text:
+    if child_count := len(todo.todos):
+        text = context["format_with_child"].format(
+            description=todo.description, child_count=child_count
+        )
+    else:
+        text = context["format"].format(description=todo.description)
+
+    return Text(text)
 
 
-def todo_effort(todo: Todo, **kwargs):
-    return str(todo.effort)
+def todo_status(todo: Todo, context: dict) -> Text:
+    status = todo.status.lower()
+    settings = context[status]
+    return Text(settings["format"], style=build_style(settings["css"]))
 
 
-def todo_urgency(todo: Todo, **kwargs):
-    return str(todo.urgency)
+def todo_urgency(todo: Todo, context: dict) -> Text:
+    if not todo.urgency:
+        return Text("")
+
+    settings = context[str(todo.urgency).lower()]
+    return Text(settings["format"], style=build_style(settings["css"]))
 
 
-def todo_due(todo: Todo, **kwargs):
-    return str(todo.due) if todo.due else ""
+def todo_due(todo: Todo, context: dict) -> Text:
+    if not todo.due:
+        return Text("")
+
+    status = todo.status
+    settings = context[status]
+    format = settings["format"]
+
+    if todo.due:
+        if todo.due.hour or todo.due.minute:
+            fmt_str = context["date_format"]
+        else:
+            fmt_str = context["date_format_with_time"]
+
+        due_str = todo.due.strftime(fmt_str)
+    else:
+        due_str = "-"
+
+    text = format.format(date=due_str)
+    return Text(text, style=build_style(settings["css"]))
 
 
-def todo_recurrence(todo: Todo, **kwargs):
-    return str(todo.recurrence) if todo.recurrence else ""
+def todo_effort(todo: Todo, context: dict) -> Text:
+    if not todo.effort:
+        return Text("")
+
+    text = context["format"].format(effort=todo.effort)
+    return Text(text, style=build_style(context["css"]))
+
+
+def todo_recurrence(todo: Todo, context: dict) -> Text:
+    if not todo.recurrence:
+        return Text("")
+
+    text = context["format"].format(recurrence=todo.recurrence)
+    return Text(text, style=build_style(context["css"]))
